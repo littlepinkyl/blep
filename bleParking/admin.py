160fc9323510a4945b671793bec387783e66b72e
littlepinkyl <little.pinky@qq.com>
2016-10-17 15:09:10 +0800

    search pk objectID, but not support non-pk objectId

--- bleParking/admin.py
+++ bleParking/admin.py
@@ -39,10 +39,49 @@ class parklotAdmin(admin.ModelAdmin):
             queryset |= self.model.objects.filter(pk_id=search_term_as_object)
         return queryset,use_distinct
 
+
     def queryset(self,request):
-        qs=super(parklotAdmin,self).queryset(request)
+        #qs=super(parklotAdmin,self).queryset(request)
+        qs = self.model._default_manager.get_query_set()
         logger.debug('heyheyhey-----{0}'.format(request.GET))
-        return qs
+        ###
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        # Custom Search
+        # 1. Get the query value and clear it from the request
+        q = ''
+        try:
+            q = request.GET['q']
+            copy = request.GET.__copy__()
+            copy.__delitem__('q')
+            request.GET = copy
+
+        except:
+            pass
+
+        result_list = []
+#
+        # Search on main model (parklot)
+        if q != '':
+            parklot_obj_list = self.model.objects.filter(description__contains=q)
+            try:
+                parklot_obj_list |= self.model.objects.filter(pk__exact=ObjectId(q))
+            except:
+                pass
+            logger.debug('heyheyhey----resultlo-{0}'.format(parklot_obj_list))
+        else:
+            parklot_obj_list = self.model.objects.all()
+        for parklot in parklot_obj_list:
+                result_list.append(parklot.pk)
+
+        # Search on the other related model (Other Name)
+        #other_names_obj_list = OtherName.objects.filter(name__contains=q)
+        #for other_name in other_names_obj_list:
+        #    result_list.append(other_name.person.pk)
+        return qs.filter(pk__in=result_list)  # apply the filter
+        #return qs
 
     def save_model(self, request, obj, form, change):
         #save with the time and the operator id
@@ -61,6 +100,49 @@ class regcheckAdmin(admin.ModelAdmin):
     fields=['pk_id','agent','parklot','car_plate','pic','state','create_on','update_on']
     list_filter=['create_on']
     #date_hierarchy='create_on'
+    def queryset(self, request):
+        # qs=super(parklotAdmin,self).queryset(request)
+        qs = self.model._default_manager.get_query_set()
+        logger.debug('heyheyhey-----{0}'.format(request.GET))
+        ###
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        # Custom Search
+        # 1. Get the query value and clear it from the request
+        q = ''
+        try:
+            q = request.GET['q']
+            copy = request.GET.__copy__()
+            copy.__delitem__('q')
+            request.GET = copy
+
+        except:
+            pass
+
+        result_list = []
+        #
+        # Search on main model (parklot)
+        if q != '':
+            regcheck_obj_list = self.m#odel.objects.filter(car_plate__contains=q)
+            try:
+                regcheck_obj_list |= self.model.objects.filter(pk__exact=ObjectId(q))
+                #regcheck_obj_list |= self.model.objects.filter(agent__exact=ObjectId(q))
+                #regcheck_obj_list |= self.model.objects.filter(parklot__exact=ObjectId(q))
+            except:
+                pass
+            logger.debug('heyheyhey----resultlo-{0}'.format(regcheck_obj_list))
+        else:
+            regcheck_obj_list = self.model.objects.all()
+        for regcheck in regcheck_obj_list:
+            result_list.append(regcheck.pk)
+
+        # Search on the other related model (Other Name)
+        # other_names_obj_list = OtherName.objects.filter(name__contains=q)
+        # for other_name in other_names_obj_list:
+        #    result_list.append(other_name.person.pk)
+        return qs.filter(pk__in=result_list)  # apply the filter
 
 class parkticketAdmin(admin.ModelAdmin):
     search_fields=['car_plate']
@@ -75,6 +157,49 @@ class parkticketAdmin(admin.ModelAdmin):
     #fields = ['car_plate', 'parklot', 'agent', 'State Details', 'create_on', 'update_on']
     list_filter=['create_on']
     #date_hierarchy='create_on'
+    def queryset(self, request):
+        # qs=super(parklotAdmin,self).queryset(request)
+        qs = self.model._default_manager.get_query_set()
+        logger.debug('heyheyhey-----{0}'.format(request.GET))
+        ###
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        # Custom Search
+        # 1. Get the query value and clear it from the request
+        q = ''
+        try:
+            q = request.GET['q']
+            copy = request.GET.__copy__()
+            copy.__delitem__('q')
+            request.GET = copy
+
+        except:
+            pass
+
+        result_list = []
+        #
+        # Search on main model (parklot)
+        if q != '':
+            parkticket_obj_list = self.model.objects.filter(car_plate__contains=q)
+            try:
+                parkticket_obj_list |= self.model.objects.filter(pk__exact=ObjectId(q))
+                #parkticket_obj_list |= self.model.objects.filter(parklot__exact=ObjectId(q))
+                #parkticket_obj_list |= self.model.objects.filter(agent__exact=ObjectId(q))
+            except:
+                pass
+            logger.debug('heyheyhey----resultlo-{0}'.format(parkticket_obj_list))
+        else:
+            parkticket_obj_list = self.model.objects.all()
+        for parkticket in parkticket_obj_list:
+            result_list.append(parkticket.pk)
+
+        # Search on the other related model (Other Name)
+        # other_names_obj_list = OtherName.objects.filter(name__contains=q)
+        # for other_name in other_names_obj_list:
+        #    result_list.append(other_name.person.pk)
+        return qs.filter(pk__in=result_list)  # apply the filter
 
 class agentAdmin(admin.ModelAdmin):
     list_display=('nick','phone','pk_id','parklot')
@@ -89,6 +214,51 @@ class agentAdmin(admin.ModelAdmin):
         if db_field.name in ['parklot']:
             formfield.widget = forms.Textarea(attrs=formfield.widget.attrs)
         return formfield
+    def queryset(self,request):
+        #qs=super(parklotAdmin,self).queryset(request)
+        qs = self.model._default_manager.get_query_set()
+        logger.debug('heyheyhey-----{0}'.format(request.GET))
+        ###
+        ordering = self.get_ordering(request)
+        if ordering:
+            qs = qs.order_by(*ordering)
+
+        # Custom Search
+        # 1. Get the query value and clear it from the request
+        q = ''
+        try:
+            q = request.GET['q']
+            copy = request.GET.__copy__()
+            copy.__delitem__('q')
+            request.GET = copy
+
+        except:
+            pass
+
+        result_list = []
+#
+        # Search on main model (parklot)
+        if q != '':
+            agent_obj_list = self.model.objects.filter(nick__contains=q)
+            agent_obj_list |= self.model.objects.filter(phone__contains=q)
+            #agent_obj_list |= self.model.objects.filter(parklot__contains=ObjectId(q))
+            try:
+                agent_obj_list |= self.model.objects.filter(pk__exact=ObjectId(q))
+
+            except:
+                pass
+            logger.debug('heyheyhey----resultlo-{0}'.format(agent_obj_list))
+        else:
+            agent_obj_list = self.model.objects.all()
+        for agent in agent_obj_list:
+                result_list.append(agent.pk)
+
+        # Search on the other related model (Other Name)
+        #other_names_obj_list = OtherName.objects.filter(name__contains=q)
+        #for other_name in other_names_obj_list:
+        #    result_list.append(other_name.person.pk)
+        return qs.filter(pk__in=result_list)  # apply the filter
+        #return qs
 
 admin.site.register(parklot, parklotAdmin)
 admin.site.register(regcheck,regcheckAdmin)
